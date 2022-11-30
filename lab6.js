6040571595142a51017302dc2f07ff01f60642dd
Алексей Бельтиков <bieltikov@bk.ru>
2022-11-30 10:45:49 +0700

    Alert

--- lab6.js
+++ lab6.js
@@ -7,6 +7,15 @@ function setRussian() {
 
     document.getElementById('your-name').innerHTML = 'Ваше имя';
     document.getElementById('input-name').placeholder = 'введите имя';
+
+    document.getElementById('your-surname').innerHTML = 'Ваша фамилия';
+    document.getElementById('input-surname').placeholder = 'Введите вашу фамилию';
+
+    document.getElementById('your-age').innerHTML = 'Ваш возраст';
+    document.getElementById('input-age').placeholder = 'Введите вашв возраст';
+
+    document.getElementById('ok-button').value = 'Ок';
+    document.getElementById('reset-button').value = 'Сброс';
 }
 
 function setEnglish() {
@@ -18,4 +27,44 @@ function setEnglish() {
 
     document.getElementById('your-name').innerHTML = 'Your name';
     document.getElementById('input-name').placeholder = 'Enter your name';
+
+    document.getElementById('your-surname').innerHTML = 'Your surname';
+    document.getElementById('input-surname').placeholder = 'Enter your surname';
+
+    document.getElementById('your-age').innerHTML = 'Your age';
+    document.getElementById('input-age').placeholder = 'Enter your age';
+
+    document.getElementById('ok-button').value = 'Ok';
+    document.getElementById('reset-button').value = 'Reset';
+}
+
+function showAlert() {
+    let userName = document.getElementById('input-name').value;
+    let userSurname = document.getElementById('input-surname').value;
+    let userAge = document.getElementById('input-age').value;
+    alert('Привет,' + ' ' + userName + ' ' + userSurname + '\n' + 'Ваш возраст:' + userAge);
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('user-data').style.backgroundColor = color;
+}
+function showAlert2(){
+let student = document.getElementById('header').value
+    alert('Барабара беребере')
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('header').style.backgroundColor = color;
+}
+
+function showAlert3(){
+    let footer = document.getElementById('footer').value
+        alert('Эй Дора готова')
+        let r = Math.round(Math.random()*255);
+        let g = Math.round(Math.random()*255);
+        let b = Math.round(Math.random()*255);
+        let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+        document.getElementById('footer').style.backgroundColor = color;
 }
