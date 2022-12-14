3ac1f3284685e67a5f4ce091ae83161931efa8c1
Алексей Бельтиков <bieltikov@bk.ru>
2022-12-14 09:28:39 +0700

    Количество дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -4,4 +4,18 @@ function showDate(params) {
     out.innerHTML = today.toLocaleDateString("ru-RU");
 }
 
+function showDaysCount(params) {
+    let today = new Date ();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let showDaysCount = (today - birthday)/1000/60/60/24;
+    let result = document.getElementById('result');
+    showDaysCount = Math.ceil(showDaysCount);
+    result.innerHTML = 'С даты рождения прошло дней: ' + showDaysCount;
+} 
+
+
+
 window.addEventListener('load', showDate);
+
+
