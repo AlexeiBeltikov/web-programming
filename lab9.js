c0baf011c5070e063baaea3c4d9431a3f5446b8f
Алексей Бельтиков <bieltikov@bk.ru>
2022-12-23 09:00:45 +0700

    Кнопка Имя-Фамилия

--- lab9.js
+++ lab9.js
@@ -4,3 +4,10 @@ function clickMe(button) {
     else
         button.innerHTML = 'Щелчок';
 }
+
+function clickMeFIO(button) {
+    if(button.innerHTML == 'Алексей')
+        button.innerHTML = 'Бельтиков';
+    else
+        button.innerHTML = 'Алексей';
+}
